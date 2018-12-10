5eb1c5687001c8c69a78665b3908dc5a69ca9d4a
sarebok <bortonae@hotmail.com>
2018-12-10 15:10:43 -0300

    Summary
    First I will create a basic "Hello world" in python... I will see what comes next

---
+++ Hello-world-in-python.py
@@ -0,0 +1,16 @@
+print("Hola")
+print("Como estas")
+print("Ok, entonces no tengo problemas para poder ejecutar el programa")
+#Now I will try to do the same but funnier#
+my_hello_list=["Hola",","," ","como"," ","estas."]
+for word in my_hello_list:
+    #recordar el "end", que permite sacar el \n al final del print
+    #Tambien existe sep y file.
+    print(word,end="")
+
+print("Ok, según entiendo, github funciona como control de cambios en carpetas locales. Cuando acepto el cambio, puedo publicarlo al repositorio online... lo cual parece bastante útil")
+print("Tal vez podría empezar a trabajar directamente en el computador en vez del IPAD... asi voy publicando mi codigo instantaneamente")
+print("""Imagino que puedo crear un repo "Ejercicios", donde guarde todos los ejercicios que voy realizando de los distintos cursos o paginas""")
+print("También podría empezar a trabajar con un editor de texto más entretenido, porque el que viene con python es horrible")
+print("De hecho, como no se ocuparlo, ni siquiera puedo ver la descripción correcta de los errores (con detalle)")
+print("probando cuando aparecen los cambios en git")
